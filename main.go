b8b61b17b6e5e40c61be8e5c8b2446988683b7cf
Andree Klattenhoff <mail@andr.ee>
2019-10-06 00:23:21 +0200

    generate private and public keys

--- main.go
+++ main.go
@@ -1,30 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "github.com/vishvananda/netlink"
-    "golang.zx2c4.com/wireguard/wgctrl"
-    "golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+	"fmt"
+	"github.com/vishvananda/netlink"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 )
 
 func main() {
-    la := netlink.NewLinkAttrs()
-    la.Name = "wg1"
-    mydev := &netlink.GenericLink{LinkAttrs: la, LinkType: "wireguard"}
-    err := netlink.LinkAdd(mydev)
-    if err != nil  {
-        fmt.Printf("could not add %s: %v\n", la.Name, err)
-    }
-    // TODO 
-    // - Generate private key
-    // - Generate public key
-    // - setup device
-    // - ip addr add
-    // - ip link up
-    // - ip route
-    // - setup peer
-    err = netlink.LinkDel(mydev)
-    if err != nil  {
-        fmt.Printf("could not del %s: %v\n", la.Name, err)
-    }
+
+	// setup wireguard generic link
+	la := netlink.NewLinkAttrs()
+	la.Name = "wg1"
+	wgdev := &netlink.GenericLink{LinkAttrs: la, LinkType: "wireguard"}
+	err := netlink.LinkAdd(wgdev)
+	if err != nil  {
+		fmt.Printf("could not add %s: %v\n", la.Name, err)
+	}
+
+	// generate private key
+	privateKey, perr := wgtypes.GeneratePrivateKey()
+	if perr != nil  {
+		fmt.Printf("could not generate private key %s: %v\n", privateKey, err)
+	}
+
+	// print out public key
+	publicKey := privateKey.PublicKey()
+	fmt.Printf("public key: %s\n", publicKey.String())
+
+	// TODO 
+	// - setup device
+	// - ip addr add
+	// - ip link up
+	// - ip route
+	// - setup peer
+	err = netlink.LinkDel(wgdev)
+	if err != nil  {
+		fmt.Printf("could not del %s: %v\n", la.Name, err)
+	}
 }
